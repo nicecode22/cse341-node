905ef3a621df6e92a3f82e9979e8573a806e3cd0
nicecode22 <nicecode22@gmail.com>
2022-01-13 15:34:52 -0600

    commit

--- section 02/routes/admin.js
+++ section 02/routes/admin.js
@@ -2,10 +2,12 @@ const path = require('path');
 
 const express = require('express');
 
+const dir = require('../help/path');
+
 const router = express.Router();
 //admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'add-product.html'));
+    res.sendFile(path.join(dir, 'views', 'add-product.html'));
 });
 //admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
