ec1261bc8aa78dd2a02ce9443db4a646e50959d4
nicecode22 <nicecode22@gmail.com>
2022-01-15 22:46:26 -0600

    commit

--- section 02/routes/shop.js
+++ section 02/routes/shop.js
@@ -3,10 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const dir = require('../help/path');
+const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
+    console.log(adminData.products);
     res.sendFile(path.join(dir, 'views', 'shop.html'));
 });
 
