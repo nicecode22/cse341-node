711ec7f274c7cc1aa88c7026cb51534585911808
nicecode22 <nicecode22@gmail.com>
2022-01-18 10:53:54 -0600

    commit

--- section 02/routes/admin.js
+++ section 02/routes/admin.js
@@ -7,13 +7,15 @@ const dir = require('../help/path');
 const router = express.Router();
 
 const products = [];
+
 //admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Product'});
+    res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product' });
 });
+
 //admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-    products.push({title: req.body.title});
+    products.push({ title: req.body.title });
     res.redirect('/');
 });
 
