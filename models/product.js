1615af9ff39a49bf4ccaa17ee245a6a43827df36
nicecode22 <nicecode22@gmail.com>
2022-01-19 16:38:02 -0600

    commit

--- models/product.js
+++ models/product.js
@@ -7,8 +7,7 @@ const p = path.join(
     'products.json'
 );
 
-const getProductsFromFile = (cb) => {
-    
+const getProductsFromFile = cb => {
     fs.readFile(p, (err, fileContent) => {
         if (err) {
             cb([]);
@@ -19,19 +18,30 @@ const getProductsFromFile = (cb) => {
 }
 
 module.exports = class Product {
-    constructor(title) {
+    constructor(title, imageUrl, price, description) {
         this.title = title;
+        this.imageUrl = imageUrl;
+        this.price = price;
+        this.description = description;
     }
 
     save() {
+        this.id = Math.random().toString();
         getProductsFromFile(products => {
             products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
+            fs.writeFile(p, JSON.stringify(products), err => {
                 console.log(err);
             });
         });
     }
     static fetchAll(cb) {
         getProductsFromFile(cb);
+    }
+
+    static findById(id, cb) {
+        getProductsFromFile(products => {
+            const product = products.find(p => p.id === id);
+            cb(product);
+        });
     }
 };
