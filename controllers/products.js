14b9c70e26dc138e7bb476b5ed1fdc63e464f5fe
nicecode22 <nicecode22@gmail.com>
2022-01-18 23:10:24 -0600

    commit

---
+++ controllers/products.js
@@ -0,0 +1,29 @@
+const Product = require('../models/product')
+
+exports.getAddProduct = (req, res, next) => {
+    res.render('add-product', { 
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product', 
+        mainCSS: true, 
+        activeProduct: true 
+    });
+};
+
+exports.postAddProduct = (req, res, next) => {
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll((products) => {
+        res.render('shop', {
+            prods: products, 
+            pageTitle: 'Shop', 
+            path: '/', 
+            hasProds: products.length > 0, 
+            activeShop: true,
+            mainCSS: true
+        });
+    });
+};
--- routes/shop.js
+++ routes/shop.js
@@ -2,21 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const dir = require('../help/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', {
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProds: products.length > 0, 
-        activeShop: true,
-        mainCSS: true
-    });
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
