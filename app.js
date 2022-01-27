dbc3521229bc26b75b56c99cf6c33de2af0ec06e
nicecode22 <nicecode22@gmail.com>
2022-01-26 21:42:41 -0600

    commit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
     User.findById('61f1f1435be3a70c19826101')
         .then(user => {
-            req.user = user;
+            req.user = new User(user.name, user.email, user.cart, user._id);
             next();
         })
         .catch(err => console.log(err));
