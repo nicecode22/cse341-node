2d5b739f1920696a2d45b94fed9611d92a3daaae
nicecode22 <nicecode22@gmail.com>
2022-01-28 12:43:20 -0600

    commit

--- models/user.js
+++ models/user.js
@@ -62,6 +62,20 @@ class User {
             });
     }
 
+    deleteCartItem(productId) {
+        const updatedCartItems = this.cart.items.filter(item => {
+            return item.productId.toString() !== productId.toString();
+        });
+        const db = getDb();
+        return db
+            .collection('users')
+            .updateOne(
+                {_id: new ObjectId(this._id)}, 
+                {$set: {cart: { items: updatedCartItems }}}
+            );
+    }
+
+
 
     static findById(userId) {
         const db = getDb();
