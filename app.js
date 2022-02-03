3b0950827d8621666079f2be641bc06e333f42ea
nicecode22 <nicecode22@gmail.com>
2022-02-03 00:07:27 -0600

    commit

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const expSession = require('express-session');
-const cmStore = require('connect-mongodb-session')(expSession);
+const session = require('express-session');
+const cmStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error404');
 const User = require('./models/user');
@@ -29,19 +29,19 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-    expSession({
+    session({
         secret: 'my secret', 
         resave: false, 
         saveUninitialized: false, 
         store: store 
     })
-);
+    );
 
 app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
     }
-    User.findById('req.session.user._id')
+    User.findById(req.session.user._id)
     .then(user => {
         req.user = user;
         next();
